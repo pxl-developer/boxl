e6591cda90d7a118ec6a2e1a48e87c37e9b16efe
PXL Developer <ti@pxlsolutions.com.br>
2023-07-06 16:03:14 -0300

    Remoção do debug

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -66,7 +66,7 @@ class RegisteredUserController extends Controller
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
-        dd($CEP);
+        
         Address::create([
             'cep' => $CEP->cep,
             'street_name' => $CEP->logradouro,
