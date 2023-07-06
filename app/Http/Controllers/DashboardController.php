0976f3d816aae465759602d255e05d8f02dc5b52
PXL Developer <ti@pxlsolutions.com.br>
2023-07-06 12:11:34 -0300

    Retorna os pedidos e pagamentos

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,24 +2,38 @@
 
 namespace App\Http\Controllers;
 
-use Inertia\Inertia;
 use App\Models\User;
+use Inertia\Inertia;
+use Inertia\Response;
 use App\Models\Order;
+use App\Models\Payment;
 use Illuminate\Http\Request;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\PaymentMethods\Pix;
 use App\Http\Controllers\PaymentMethods\MakePayment;
 
 class DashboardController extends Controller
 {
-    public function index()
+    public function index(): Response
     {
+        $orders = Order::where('id_user', Auth::user()->id)
+            ->where('order_status', 'PEDIDO_ABERTO')
+            ->get();
+
+        $payments = Payment::whereHas('order', function ($query) {
+                $query->where('id_user', Auth::user()->id);
+            })
+            ->where('transaction_status', 'pending')
+            ->get();
+
         return Inertia::render('Dashboard', [
-            'orders' => Order::where('id_user', Auth::user()->id)->get(),
+            'orders' => $orders,
+            'payments' => $payments
         ]);
     }
     
-    public function pay(Request $request)
+    public function pay(Request $request): void
     {
         $createPayment = new MakePayment(new Pix());
         
@@ -27,9 +41,5 @@ class DashboardController extends Controller
             User::find(Auth::user()->id)->with('address')->get()->first(),
             Order::find($request->orderID)
         );
-    }
-
-    public function wallet(){
-        return Inertia::render('Wallet');
     }
 }
